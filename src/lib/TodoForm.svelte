f2589d218ec08d3b3aeba0f2557b15c03ab4e91e
Steve Jeong <jst0930@gmail.com>
2022-04-19 19:44:20 +0300

    completed

---
+++ src/lib/TodoForm.svelte
@@ -0,0 +1,32 @@
+<script>
+  let todo = '';
+  import {addTodo} from '../stores/todoStore'
+  const handleSubmit = () => {
+    addTodo(todo);
+    todo = ''
+    console.log('Submitting')
+  }
+</script>
+<form 
+  class='my-6' 
+  action="/" 
+  on:submit|preventDefault={handleSubmit}>
+  <div 
+    class='flex flex-col text-sm mb-2'>
+    <label 
+      class='font-bold mb-2 text-green-800' 
+      for="todo">Todo
+    </label>
+    <input 
+      class='appearance-none shadow-sm border border-gray-200 p-2 focus:outline-none focus:border-gray-500 rounded-lg' 
+      type='text' 
+      name='todo' 
+      placeholder="Watcha gotta do?" 
+      bind:value={todo} 
+    />
+  </div>
+  <button 
+    class='w-full shadow-sm rounded bg-blue-500 hover:border-blue-100 text-white py-2 px-4'
+    type='submit'>Submit
+  </button>
+</form>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,12 @@
+<script>
+  import TodoForm from '$lib/TodoForm.svelte'
+  import Todo from '$lib/Todo.svelte'
+  import {todos} from '../stores/todoStore.js'
+</script>
 <main>
-  <h1 class="text-2xl font-bold text-center text-gray-80 md:text-4xl">My Todos</h1>
+  <h1 class="text-2xl font-bold text-center text-gray-700 md:text-4xl">My Todos</h1>
+  <TodoForm />
+  {#each $todos as todo}
+    <Todo todo={todo} index={todo.id} />
+  {/each}
 </main>
